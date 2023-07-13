5ee6f67c849b4e93a82a7e702ae010ae16ded97d
jonandgon <jag73155@usc.edu>
2023-07-12 20:42:20 -0400

    remove connection references

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-import {
-  ConnectMessage,
-  ConnectRejMessage,
-  ConnectResMessage,
-} from './messaging/connect.js';
-
-export type MessageType =
-  | ConnectMessage
-  | ConnectResMessage
-  | ConnectRejMessage;
-
-export type WindowMessageType = MessageType & {
-  forwarded?: boolean;
-  extensionId: string;
-};
-
 export * from './web3modal.js';
 export * from './provider/provider.js';
 export * from './hooks/index.js';
@@ -21,6 +5,4 @@ export * from './data/asset.js';
 export * from './data/types.js';
 export * from './data/walletconnect.js';
 export * from './data/coins/dai.js';
-export * from './messaging/connect.js';
 export * from './messaging/walletconnecterror.js'
-export * from './client/puzzle_wallet_client.js'
