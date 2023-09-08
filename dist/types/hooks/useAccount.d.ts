79b58f070751d2b30d19e91220aa02c2e900ac0e
jonandgon <jag73155@usc.edu>
2023-09-08 18:48:55 -0500

    remove dist

--- dist/types/hooks/useAccount.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const shortenAddress: (address: string) => string;
-export declare const useAccount: () => {
-    account: import("../index.js").PuzzleAccount | undefined;
-    accounts: import("../index.js").PuzzleAccount[];
-    error: string | undefined;
-    loading: any;
-};
--- dist/types/hooks/useDecrypt.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const useDecrypt: (transactionId?: string) => {
-    decrypt: () => void;
-    data: import("../index.js").DeployResData | import("../index.js").ExecuteResData | undefined;
-    loading: any;
-    error: string | undefined;
-};
