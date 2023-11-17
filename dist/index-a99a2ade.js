678d8a08d0a17ff22478ebbafc9f1cfa6f4d2fdb
Luke Newman <5315645+lukenewman@users.noreply.github.com>
2023-11-18 02:32:54 +0300

    any to map and bump to 26

--- dist/index-a99a2ade.js
+++ dist/index-a99a2ade.js
@@ -1,5 +1,5 @@
 import $i, { useDebugValue as Uu, useEffect as yr, useState as xr } from "react";
-const Fu = "@puzzlehq/sdk", Mu = "Puzzle SDK", $u = "0.1.25", ju = "Your portal to privacy", qu = "./dist/puzzle.umd.js", zu = "./dist/puzzle.es.js", Bu = "./dist/types/src/index.d.ts", Ku = {
+const Fu = "@puzzlehq/sdk", Mu = "Puzzle SDK", $u = "0.1.26", ju = "Your portal to privacy", qu = "./dist/puzzle.umd.js", zu = "./dist/puzzle.es.js", Bu = "./dist/types/src/index.d.ts", Ku = {
   ".": {
     import: "./dist/puzzle.es.js",
     require: "./dist/puzzle.umd.js",
@@ -449,7 +449,7 @@ let El = class {
   }
   async initUi() {
     if (typeof window < "u") {
-      await import("./index-5c61c766.js");
+      await import("./index-4f09f310.js");
       const e = document.createElement("wcm-modal");
       document.body.insertAdjacentElement("beforeend", e), Pt.setIsUiLoaded(!0);
     }
