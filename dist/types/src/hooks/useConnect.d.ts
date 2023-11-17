183be8bd61db9b702f0daf1d5e985242b52bfbea
jonandgon <jag73155@usc.edu>
2023-11-18 02:23:57 +0300

    remove dist

--- dist/types/src/hooks/useConnect.d.ts
+++
@@ -1,7 +0,0 @@
-import { SessionTypes } from '@walletconnect/types';
-export declare function useConnect(): {
-    data: SessionTypes.Struct;
-    error: string;
-    loading: boolean;
-    connect: () => Promise<any>;
-};
