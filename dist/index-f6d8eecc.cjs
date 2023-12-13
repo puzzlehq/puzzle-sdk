9a7900cccdcd71e2226117a2ab923b371ea669bb
jonandgon <jag73155@usc.edu>
2023-12-13 15:47:06 -0500

    0.1.44

--- dist/index-f6d8eecc.cjs
+++ dist/index-f6d8eecc.cjs
@@ -1,4 +1,4 @@
-"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const d=require("./index-889959b5.cjs");require("react");/**
+"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const d=require("./index-b375cd3f.cjs");require("react");/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
