ab828073c0894c564f73058b8c31afaf9bb52519
jonandgon <jonathan@puzzle.online>
2024-03-09 15:35:22 -0500

    remove dist temporarily

--- packages/core/dist/types/src/data/events.d.ts
+++
@@ -1,7 +0,0 @@
-import { Balance } from '../messages/balance.js';
-export type AccountSelectedResponse = {
-    address: string;
-    balances: Balance[];
-    chain?: string;
-};
-export type AccountSyncedResponse = AccountSelectedResponse;
--- packages/react/dist/types/react/src/hooks/useRequestSignature.d.ts
+++
@@ -1,7 +0,0 @@
-import { SignatureResponse } from '@puzzlehq/sdk-core';
-export declare const useRequestSignature: (message: string, address?: string) => {
-    requestSignature: () => void;
-    response: SignatureResponse | undefined;
-    loading: boolean;
-    error: string | undefined;
-};
